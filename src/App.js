06c63f6ccecdcb1b640c4065aeec6c3102809866
Ana Fretes <anafretes@gmail.com>
2024-03-19 01:29:35 -0300

    Added footnote

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <p>
+        <small>
+          <a
+            href="https://github.com/anafretes/react-week4-weather"
+            target="_blank"
+            rel="noreferrer">
+            Open-sourced code
+          </a>
+          , built by <a href="mailto:home@derpycomputer.com">Ana Fretes</a> for{" "}
+          <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
+            SheCodes
+          </a>
+        </small>
+      </p>
     </div>
   );
 }
